0797d2383dabbe3b9e849e127e7448a840096d7b
Polina <54813973+fokicheva@users.noreply.github.com>
2020-03-02 18:10:20 +0000

    Initial commit

---
+++ platforms/android/app/src/main/assets/www/precache-manifest.093f676afc474de9fbec04a9e6614cc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11195344b0dfacec20d817f07f7a01bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c952f4d88acea436642",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7e49105eefc84df5f174",
+    "url": "./static/js/2.5dcd050f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.5dcd050f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c952f4d88acea436642",
+    "url": "./static/js/main.3ab15f40.chunk.js"
+  },
+  {
+    "revision": "d838022f889c0910cb35",
+    "url": "./static/js/runtime-main.fc2c2ff8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
