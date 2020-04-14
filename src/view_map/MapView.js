475029e0ed86d4427d50fc6da80346817ac59172
Polina <54813973+fokicheva@users.noreply.github.com>
2020-04-15 00:22:50 +0100

    Revert "."
    This reverts commit 932a1dbc7a8d685964c91761e9d4fa62320e6e5f.

--- src/view_map/MapView.js
+++ src/view_map/MapView.js
@@ -1,13 +1,8 @@
 import React, {Component} from 'react';
+import {
+    Link
+} from "react-router-dom";
 import '../App.css';
-
-//import files
-import poster from "../img/UI/poster.png";
-import img_1 from "../../www/demos";
-import img_2 from "../img/UI/demos/2.PNG";
-import img_3 from "../img/UI/demos/3.PNG";
-import img_4 from "../img/UI/demos/4.PNG";
-import img_5 from "../img/UI/demos/5.PNG";
 
 class MapView extends Component {
     constructor(props){
@@ -19,26 +14,7 @@ class MapView extends Component {
         return (
             <div className="standardView">
                 <div id="mapView">
-                    <img src={img_1}/>
-                    <div id="mapStart">
-                        <div id="mapTop">
-                            This app is an early prototype! Map coming in the future.
-                            <br/>
-                            Demos:
-                        </div>
-                        <div id="mapDemos">
-                            {/*<img src={poster}/>*/}
-                            {/*<img src={img_1}/>*/}
-                            {/*<img src={img_2}/>*/}
-                            {/*<img src={img_3}/>*/}
-                            {/*<img src={img_4}/>*/}
-                            {/*<img src={img_5}/>*/}
-                        </div>
-                        <div id="mapBottom">
-                            Scroll sideways for more demo shots.
-                        </div>
-                    </div>
-
+                    <Link to="/">Quests</Link>
                 </div>
             </div>
 
@@ -46,8 +22,8 @@ class MapView extends Component {
     }
 
     componentDidMount(){
-        let body_height = "height: " + (parseInt(document.body.scrollHeight) - (2*parseInt(getComputedStyle(document.getElementById('mapView'),null).getPropertyValue('border-width')))) + "px";
-        //let body_height = "height: " + parseInt(document.body.scrollHeight) + "px";
+        //let body_height = "height: " + (parseInt(document.body.scrollHeight) - (2*parseInt(getComputedStyle(document.getElementById('questView'),null).getPropertyValue('border-width')))) + "px";
+        let body_height = "height: " + parseInt(document.body.scrollHeight) + "px";
         document.getElementById("mapView").setAttribute("style", body_height);
     }
 
---
+++ www/precache-manifest.b1a265607e2176081b15aeb74cb7af37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57a291af7c200837ba7939abd4e6f5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1d0ebbca19e8b03afed",
+    "url": "./static/css/main.40405145.chunk.css"
+  },
+  {
+    "revision": "8d899dfdb194203500bd",
+    "url": "./static/js/2.a590f775.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.a590f775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d0ebbca19e8b03afed",
+    "url": "./static/js/main.8ed04f5f.chunk.js"
+  },
+  {
+    "revision": "d838022f889c0910cb35",
+    "url": "./static/js/runtime-main.fc2c2ff8.js"
+  },
+  {
+    "revision": "bae7c83c5196cdf24c7babd33d63efe6",
+    "url": "./static/media/8bitoperator.bae7c83c.ttf"
+  }
+]);
